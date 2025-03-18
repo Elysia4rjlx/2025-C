f84b874ae388194492801c80350f2a816630fb13
Elysia4rjlx <2811197713@qq.com>
2025-03-18 21:40:48 +0800

    Add files via upload

---
+++ 2025_03_17数组练习1/1.c
@@ -0,0 +1,27 @@
+//////练习1:多个字符从两端移动, 向中间汇聚编写代码, 
+//////       演示多个字符从两端移动, 向中间汇聚
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	char arr1[] = "I miss you. You know?";
+//	char arr2[] = "#####################";
+//
+//	int left = 0 ;
+//	int right = strlen(arr1) - 1;
+//	for (left = 0; left <= right; left++, right--)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		Sleep(100);
+//		system("cls");
+//	}
+//	printf("%s\n", arr2);
+//
+//
+//	return 0;
+//}
+//
