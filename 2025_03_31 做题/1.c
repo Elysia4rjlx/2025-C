7ec2839ced9de26f24ce33a2539ba8383b189745
Elysia4rjlx <2811197713@qq.com>
2025-03-31 23:24:38 +0800

    Add files via upload

---
+++ 2025_03_31 做题/1.c
@@ -0,0 +1,76 @@
+//#include <stdio.h>
+//int main()
+//{
+//    printf("Practice makes perfect!");
+//    return 0;
+//}
+//
+//
+//
+//#include<stdio.h>
+//int main()
+//{
+//    printf("v   v\n");
+//    printf(" v v\n");
+//    printf("  v\n");
+//    return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//
+//    printf("The size of short is %d bytes.\n", sizeof(short));
+//    printf("The size of int is %d bytes.\n", sizeof(int));
+//    printf("The size of long is %d bytes.\n", sizeof(long));
+//    printf("The size of long long is %d bytes.\n", sizeof(long long));
+//
+//    return 0;
+//}
+//
+//
+//#include <stdio.h>
+//int main()
+//{
+//    printf("     **     \n");
+//    printf("     **     \n");
+//    printf("************\n");
+//    printf("************\n");
+//    printf("    *  *    \n");
+//    printf("    *  *    \n");
+//    return 0;
+//}
+//
+//
+//
+//#include <stdio.h>
+//int main() {
+//
+//    printf("%#o %#X\n", 1234, 1234);
+//
+//    return 0;
+//}
+//
+//
+//
+//#include <stdio.h>
+//int main()
+//{
+//    int num = 0xABCDEF;
+//    printf("%15d", num);
+//    return 0;
+//}
+//
+////十六进制表示：在C语言中，十六进制数以0x或0X开头，
+////例如0xABCDEF。十六进制数是基于16的数制，每一位的权重是16的幂次。
+//
+//
+//#include <stdio.h>
+//int main()
+//{
+//
+//    int i = printf("Hello world!");
+//    printf("\n");
+//    printf("%d", i);
+//    return 0;
+//}
